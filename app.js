43f3634bf69847466129d2e1c5159777fc4b56bd
Julio Valencia <valenciagranadaj@gmail.com>
2025-01-27 14:35:01 -0600

    Array para almacenar amigos

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Crear un array para almacenar los nombres
+
+let amigos = [];
+
+//Implementa la función para agregar amigos
+
+
+//Implementa la función para actualizar la lista de amigos
+
+
+//Implementa la función para sortear los amigos
