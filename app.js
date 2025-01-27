be1da90b4febeb624252a6588b16f5e0e3c9b5cd
Julio Valencia <valenciagranadaj@gmail.com>
2025-01-27 14:37:18 -0600

    Agrega y muestra amigos

--- app.js
+++ app.js
@@ -6,8 +6,33 @@ let amigos = [];
 
 //Implementa la función para agregar amigos
 
+function agregarAmigo(){
+    const inputAmigo = document.getElementById("amigo");
+    const nombre = inputAmigo.value.trim();
+
+    if(nombre === ""){
+        alert("Debes ingresar un nombre");
+        return;
+    }
+
+    amigos.push(nombre);
+
+    inputAmigo.value = "";
+
+    mostrarLista();
+}
 
 //Implementa la función para actualizar la lista de amigos
 
+function mostrarLista(){
+    const listaAmigos = document.getElementById("lista-amigos");
+    listaAmigos.innerHTML = "";
+
+    amigos.forEach((amigo, index) => {
+        const li = document.createElement("li");
+        li.textContent = `${index + 1}. ${amigo}`;
+        listaAmigos.appendChild(li);
+    });
+}
 
 //Implementa la función para sortear los amigos
